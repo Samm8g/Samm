52316d136310764edb5914ed3919d2642dea801a
Samm <samm8gg@gmail.com>
2025-08-14 16:55:53 +0300

    conf.lua

---
+++ conf.lua
@@ -0,0 +1,9 @@
+function love.conf(t)
+    t.window.title = "Samm"
+    t.modules.timer = true
+    t.window.width = 1000
+    t.window.height = 1000
+    t.window.resizable = true
+    t.window.minwidth = 100
+    t.window.minheight = 100
+end
